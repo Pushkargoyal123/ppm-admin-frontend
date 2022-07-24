becaaa20d587059811643db00428273c7b1ef9a1
pushkargoyal123 <pushkargoyal36@gmail.com>
2022-07-24 18:17:01 +0530

    commiting

--- src/pages/Plan_details/AddFeature.js
+++ src/pages/Plan_details/AddFeature.js
@@ -1,4 +1,4 @@
-import { Box, TextField, Button } from "@material-ui/core"
+import { Box, TextField, Button, MenuItem, Select } from "@material-ui/core"
 import React, { useEffect, useState } from "react"
 import MUIDataTable from "mui-datatables";
 import { getRequestWithFetch, postRequestWithFetch } from "../../service";
@@ -7,9 +7,11 @@ export default function AddFeature() {
 
     const [rows, setRows] = useState([]);
     const [featureName, setFeatureName] = useState('');
+    const [plans, setPlans] = useState([]);
 
     useEffect(() => {
         handleList();
+        planList();
     }, [])
 
     const handleList = async () => {
@@ -17,11 +19,31 @@ export default function AddFeature() {
         setRows(res.data)
     }
 
+    const planList = async () => {
+        const data = await getRequestWithFetch("plans/planList");
+        if (data.success) {
+            data.data.forEach(function (plan) {
+                plan.planFeature = "YES";
+            })
+            setPlans(data.data);
+        }
+    }
+
     const handleAdd = async () => {
         const body = {
             featureName: featureName
         }
         await postRequestWithFetch("plans/addFeature", body)
+
+        plans.forEach(async function (plan) {
+            const body = {
+                ppmSubscriptionPlanId: plan.id,
+                featureValue: plan.planFeature,
+                featureName: featureName
+            }
+            await postRequestWithFetch("plans/addPlanFeatureList", body)
+        })
+
         setFeatureName('');
         handleList();
     }
@@ -45,6 +67,15 @@ export default function AddFeature() {
 
     })
 
+    const handleChange = (value, index) => {
+        plans.forEach(function (item, itemIndex) {
+            if (itemIndex === index) {
+                item.planFeature = value
+            }
+        })
+        setPlans(plans)
+    }
+
     const options = {
         filterType: 'checkbox',
     };
@@ -53,15 +84,34 @@ export default function AddFeature() {
         <>
             <Box component="span" style={{ padding: "1rem", margin: 'auto' }}>
                 <div>
-                    <form>
-                        <div>
-                            <TextField onChange={(e) => setFeatureName(e.target.value)} value={featureName} id="outlined-basic" label="Feature Name" style={{ width: '30rem', }} variant="outlined" />
-                            <Button onClick={() => handleAdd()} variant="contained" color="primary" style={{ height: '55px', borderRadius: '0px' }}>
-                                Add Feature
-                            </Button>
-                        </div>
-                    </form>
+                    <div>
+                        <TextField onChange={(e) => setFeatureName(e.target.value)} value={featureName} id="outlined-basic" label="Feature Name" style={{ width: '30rem', }} variant="outlined" />
+                        <Button onClick={() => handleAdd()} variant="contained" color="primary" style={{ height: '55px', borderRadius: '0px' }}>
+                            Add Feature
+                        </Button>
+                    </div>
                 </div>
+                {
+                    plans.map(function (item, index) {
+                        return <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                            <div
+                                style={{ margin: 20, fontSize: 16, border: "1px black solid", padding: "5px 30px" }}>{item.planName}</div>
+                            <div style={{ margin: 20 }}>
+                                <Select
+                                    labelId="demo-simple-select-outlined-label"
+                                    id="demo-simple-select-outlined"
+                                    label="status"
+                                    defaultValue={item.planFeature}
+                                    onChange={(event) => handleChange(event.target.value, index)}
+                                >
+                                    <MenuItem value={"YES"}>YES</MenuItem>
+                                    <MenuItem value={"NO"}>NO</MenuItem>
+                                    <MenuItem value={"OTHER"}>OTHER</MenuItem>
+                                </Select>
+                            </div>
+                        </div>
+                    })
+                }
             </Box>
             <Box component="span" style={{ marginTop: '0%', padding: "1rem", margin: 'auto' }}>
                 <MUIDataTable
