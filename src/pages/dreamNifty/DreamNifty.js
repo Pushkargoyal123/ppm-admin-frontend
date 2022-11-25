e90f32477b40ae6f26cfbd0e51c062dcf65098a7
suraj266 <surajojha2804@gmail.com>
2022-11-25 13:13:24 +0530

    event status update

--- src/pages/dreamNifty/DreamNifty.js
+++ src/pages/dreamNifty/DreamNifty.js
@@ -2,21 +2,34 @@
 import React from 'react';
 import MUIDataTable from 'mui-datatables';
 import { useEffect, useState } from 'react';
-import { Button, IconButton } from '@material-ui/core';
+import { Button, IconButton, Input, Select } from '@material-ui/core';
 import { Redeem } from '@material-ui/icons';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import parse from 'html-react-parser'
-
+import useStyles from "../dashboard/styles.js";
 
 // internal dependecies
 import { postRequestWithFetch } from '../../service';
 import CreateEvent from './CreatEvent';
 import EditEvent from './EditEvent';
 import PrizeDistribution from './PrizeDistribution';
+import { Chip, MenuItem } from '@mui/material';
+import { notifyError, notifySuccess } from '../../components/notify/Notify';
+
+
+const states = {
+  active: "success",
+  inactive: "warning",
+  deleted: "default",
+};
+
+
 
 // Dream Nifty component for showing, adding and updating all events
 export default function DreamNifty() {
+
+  let classes = useStyles();
 
   const [eventList, setEventList] = useState([]); // state for setting and showing all events 
   const [openAddModal, setOpenAddModal] = useState(false); // state for open or close add event modal
@@ -52,6 +65,23 @@ export default function DreamNifty() {
     setClickedEventName(eventName);
   }
 
+  // function to update status of event
+
+  const handleUpdateStatus = async (eventId, event) => {
+    const status = event.target.value;
+    console.table({ eventId, status })
+    // const res = await postRequestWithFetch(`user/update/${userId}`, {
+    //   status: event.target.value
+    // })
+    // if (res.success === true) {
+    //   fetchDreamNifty();
+    //   notifySuccess({ Message: "Status Updated Successfully.", ProgressBarHide: true })
+    // } else {
+    //   notifyError({ Message: "Oops! Some error occurred.", ProgressBarHide: true })
+    // }
+  }
+
+
   /**
    * function for listing all events
    */
@@ -59,6 +89,7 @@ export default function DreamNifty() {
     const data = await postRequestWithFetch("dreamNifty/eventList", {});
     if (data.success) {
       const finalData = data.data.map(function (item, index) {
+        const status = item.status;
         item.SNO = index + 1;
         item.date = <div>
           {item.startDate}
@@ -72,6 +103,25 @@ export default function DreamNifty() {
           <b>Entry Fee :</b> {item.entryFee}<br />
           <b>Virtual Amount :</b> {item.virtualAmount}<br />
         </div>
+
+
+        item.status = <Select
+          labelId="demo-mutiple-checkbox-label"
+          id="demo-mutiple-checkbox"
+          value={status}
+          onChange={(event) => { handleUpdateStatus(item.id, event) }}
+          input={<Input />}
+          renderValue={(selected) => <Chip label={selected} classes={{ root: classes[states[status.toLowerCase()]] }} />}
+        >
+          {["active", "inactive", "deleted"].map(
+            (changeStatus) => (
+              <MenuItem key={changeStatus} value={changeStatus}>
+                <Chip label={changeStatus} classes={{ root: classes[states[changeStatus.toLowerCase()]] }} />
+              </MenuItem>
+            )
+          )}
+        </Select>
+
         item.Action = <>
           <IconButton onClick={() => handleOpenEditModal(item.id, item.title)} aria-label="Edit">
             <EditIcon />
@@ -141,7 +191,7 @@ export default function DreamNifty() {
     <PrizeDistribution
       open={openPrizeModal}
       eventId={clickedEventId}
-      eventName = {clickedEventName}
+      eventName={clickedEventName}
       setOpen={setOpenPrizeModal}
     />
   </>)
